eb711dddae8dbd6a65f18e9569a53d76442f12c4
adamdeguire <adeguire443@gmail.com>
2021-08-31 01:12:24 -0500

    Add language type to post

--- app/models/log.js
+++ app/models/log.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const logSchema = new mongoose.Schema({
   title: String,
   content: String,
+  language: String,
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
